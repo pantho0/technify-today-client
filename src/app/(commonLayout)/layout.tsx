5a736fe4d0734b7c13ab8afc3fbe26734c4eff28
pantho0 <panthomashreky6@gmail.com>
2025-02-09 00:23:10 +0600

    seperated the common layout from root to group route

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,10 +1,25 @@
+import { Navbar } from "@/src/components/navbar";
+import { Link } from "@heroui/link";
 import { ReactNode } from "react";
 
 const layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
-      <p>in the common</p>
-      {children}
+    <div className="relative flex flex-col h-screen">
+      <Navbar />
+      <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+        {children}
+      </main>
+      <footer className="w-full flex items-center justify-center py-3">
+        <Link
+          isExternal
+          className="flex items-center gap-1 text-current"
+          href="https://heroui.com?utm_source=next-app-template"
+          title="heroui.com homepage"
+        >
+          <span className="text-default-600">Powered by</span>
+          <p className="text-primary">HeroUI</p>
+        </Link>
+      </footer>
     </div>
   );
 };
