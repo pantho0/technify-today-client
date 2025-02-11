482987bbdc3f15b4d33aa31f42474b02bc4fd396
pantho0 <panthomashreky6@gmail.com>
2025-02-12 01:52:27 +0600

    post card design marked

--- src/app/(commonLayout)/(home)/@latestPost/page.tsx
+++ src/app/(commonLayout)/(home)/@latestPost/page.tsx
@@ -1,12 +1,16 @@
+import PostCard from "@/src/components/home/PostCard";
 import getLatestPosts from "@/src/services/latestPosts";
 
 const LatestPost = async () => {
   const { data: posts } = await getLatestPosts();
-  console.log(posts.result);
+
   return (
     <div className="py-10">
       <div className="border-l-5 border-primary pl-2">
         <h1 className="text-2xl font-semibold ">Latest Post</h1>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {posts?.result?.map((post) => <PostCard key={post?._id} post={post} />)}
       </div>
     </div>
   );
---
+++ src/components/home/PostCard.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable react/jsx-sort-props */
+import { Button } from "@heroui/button";
+import { Card, CardBody, CardFooter } from "@heroui/card";
+import { User } from "@heroui/user";
+import moment from "moment";
+
+const PostCard = ({ post }) => {
+  return (
+    <Card className="py-4">
+      <CardBody className="overflow-visible">
+        <div className="space-y-6">
+          <img className="rounded-md" src={post?.image} alt={post?.title} />
+
+          <div className="bg-primary/30 inset-0 backdrop-blur-md inline-block px-3 py-1 rounded-md">
+            <h3 className="inline-block text-primary text-[14px] font-semibold">
+              {post?.category}
+            </h3>
+          </div>
+
+          <div>
+            <p className="text-2xl font-semibold">{post?.title}</p>
+          </div>
+        </div>
+      </CardBody>
+      <CardFooter className="justify-between">
+        <User
+          avatarProps={{
+            src: "https://i.pravatar.cc/150?u=a04258114e29026702d",
+          }}
+          name="Jane Doe"
+        />
+        <p>{moment(post?.createdAt).format("LL")}</p>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default PostCard;
