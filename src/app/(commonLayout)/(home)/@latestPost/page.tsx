6adbda45e66ba7cf2e96c658f710370455705cec
pantho0 <panthomashreky6@gmail.com>
2025-02-12 23:01:21 +0600

    spacing added to post ui

--- src/app/(commonLayout)/(home)/@latestPost/page.tsx
+++ src/app/(commonLayout)/(home)/@latestPost/page.tsx
@@ -5,11 +5,11 @@ const LatestPost = async () => {
   const { data: posts } = await getLatestPosts();
 
   return (
-    <div className="py-10">
+    <div className="py-10 my-8">
       <div className="border-l-5 border-primary pl-2">
         <h1 className="text-2xl font-semibold ">Latest Post</h1>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-6">
         {posts?.result?.map((post) => <PostCard key={post?._id} post={post} />)}
       </div>
     </div>
