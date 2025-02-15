28d288449b0c183a8bf9d2c755d7ef270ba89d0d
pantho0 <panthomashreky6@gmail.com>
2025-02-15 16:09:31 +0600

    axios instance created and added to auth services for login service

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "@/src/styles/globals.css";
 import { Metadata, Viewport } from "next";
 import clsx from "clsx";
 
-import { Providers } from "./providers";
+import { Providers } from "../lib/Providers/providers";
 
 import { siteConfig } from "@/src/config/site";
 import { fontSans } from "@/src/config/fonts";
@@ -36,7 +36,7 @@ export default function RootLayout({
       <body
         className={clsx(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable,
+          fontSans.variable
         )}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
---
+++ src/services/auth/index.ts
@@ -0,0 +1,19 @@
+import { cookies } from "next/headers";
+import { FieldValues } from "react-hook-form";
+
+import axiosInstance from "@/src/lib/AxiosInstance";
+
+const loginUser = async (userData: FieldValues) => {
+  try {
+    const { data } = await axiosInstance.post("/auth/login", userData);
+
+    if (data.success) {
+      (await cookies()).set("accessToken", data.accessToken);
+      (await cookies()).set("refreshToken", data.refreshToken);
+    }
+
+    return data;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
