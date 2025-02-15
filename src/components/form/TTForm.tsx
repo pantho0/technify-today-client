ae2266c559b9ff2dd482e517b9e92af0deb71e23
pantho0 <panthomashreky6@gmail.com>
2025-02-15 12:49:49 +0600

    global form added with formprovider

---
+++ src/components/form/TTForm.tsx
@@ -0,0 +1,39 @@
+import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
+
+interface FormConfig {
+  defaultValues?: Record<string, any>;
+  resolver?: any;
+}
+
+interface TTFormProps extends FormConfig {
+  children: React.ReactNode;
+  onSubmit: SubmitHandler<any>;
+}
+
+const TTForm = ({
+  children,
+  onSubmit,
+  defaultValues,
+  resolver,
+}: TTFormProps) => {
+  const formConfig: FormConfig = {};
+
+  if (!!defaultValues) {
+    formConfig["defaultValues"] = defaultValues;
+  }
+
+  if (!!resolver) {
+    formConfig["resolver"] = resolver;
+  }
+
+  const methods = useForm(formConfig);
+  const handleSubmit = methods.handleSubmit;
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(onSubmit)}>{children}</form>
+    </FormProvider>
+  );
+};
+
+export default TTForm;
