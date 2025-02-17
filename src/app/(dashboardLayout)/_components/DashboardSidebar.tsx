a88db0151d4c5debbabdc61516475cdb02a38309
pantho0 <panthomashreky6@gmail.com>
2025-02-17 14:15:53 +0600

    logout and themeswitcher added to dashboard sidebar

--- src/app/(dashboardLayout)/_components/DashboardSidebar.tsx
+++ src/app/(dashboardLayout)/_components/DashboardSidebar.tsx
@@ -1,5 +1,64 @@
+import { ThemeSwitch } from "@/src/components/theme-switch";
+import { useUser } from "@/src/context/user.provider";
+import { logoutUser } from "@/src/services/auth";
+import { Button } from "@heroui/button";
+import { Divider } from "@heroui/divider";
+import { Link } from "@heroui/link";
+import { Tooltip } from "@heroui/tooltip";
+import { HomeIcon, LogOut, SettingsIcon, Thermometer } from "lucide-react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
 const DashboardSidebar = () => {
-  return <div className="p-5">Dashboard</div>;
+  const router = useRouter();
+  const { setLoading } = useUser();
+
+  const handleLogout = () => {
+    logoutUser();
+    setLoading(true);
+    router.push("/login");
+  };
+
+  return (
+    <div className="">
+      <div className="flex items-center gap-2 p-5">
+        <Link href="/" className="text-inherit">
+          <Image alt="logo" height={32} src="/logo.png" width={32} />
+          <h1 className="text-xl font-bold ml-2">Technify Today</h1>
+        </Link>
+      </div>
+
+      <Divider className="my-3" />
+      <div className="flex items-center justify-center gap-3 p-1">
+        <Tooltip content="Theme Switch">
+          <ThemeSwitch />
+        </Tooltip>
+        <Tooltip content="Log out">
+          <LogOut
+            className="w-5 h-5 mr-2 cursor-pointer"
+            onClick={handleLogout}
+          />
+        </Tooltip>
+      </div>
+
+      <Divider className="my-3" />
+
+      <div className="flex flex-col gap-2 p-5">
+        <Link href="/dashboard">
+          <Button variant="ghost" className="w-full justify-start">
+            <HomeIcon className="w-5 h-5 mr-2" />
+            Dashboard
+          </Button>
+        </Link>
+        <Link href="/dashboard/settings">
+          <Button variant="ghost" className="w-full justify-start">
+            <SettingsIcon className="w-5 h-5 mr-2" />
+            Settings
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardSidebar;
