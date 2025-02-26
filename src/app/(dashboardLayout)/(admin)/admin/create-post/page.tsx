609c7db86c63074d3a2992dd9675fdd1efeb484a
pantho0 <panthomashreky6@gmail.com>
2025-02-26 16:48:20 +0600

    image upload via input and show in page functionalites are added

--- src/app/(dashboardLayout)/(admin)/admin/create-post/page.tsx
+++ src/app/(dashboardLayout)/(admin)/admin/create-post/page.tsx
@@ -1,20 +1,40 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 "use client";
 
 import { Button } from "@heroui/button";
+import { useState } from "react";
 
 import TTForm from "@/src/components/form/TTForm";
 import TTInput from "@/src/components/form/TTInput";
 import TTSelect from "@/src/components/form/TTSelect";
 import { postCategories, postStatus } from "@/src/constants";
 import CkEditor from "@/src/components/form/CkEditor";
-import { useState } from "react";
 
 const CreatePostPage = () => {
   const [editorData, setEditorData] = useState<string>("");
   const [data, setData] = useState<string>("");
 
+  const [imageFile, setImageFile] = useState<File | null>(null);
+  const [imagePreview, setImagePreview] = useState<string>("");
+  console.log(imagePreview);
+
+  const handleImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files![0];
+
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        setImagePreview(reader.result as string);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
   const handleOnUpdate = (editor: string, field: string): void => {
     if (field === "description") {
+      console.log("Editor data field:", editor);
       console.log("Editor data field:", editor);
       setData(editor);
     }
@@ -51,14 +71,39 @@ const CreatePostPage = () => {
 
           <CkEditor
             editorData={editorData}
-            setEditorData={setEditorData}
             handleOnUpdate={handleOnUpdate}
+            setEditorData={setEditorData}
           />
 
           <div
-            className="text-black"
             dangerouslySetInnerHTML={{ __html: data }}
+            className="text-black"
           />
+          <div>
+            {imagePreview && (
+              <div>
+                <div className="relative size-48 rounded-xl border-2 border-dashed p-2">
+                  <img
+                    alt="image"
+                    className="w-full h-full object-cover rounded-xl"
+                    src={imagePreview}
+                  />
+                </div>
+              </div>
+            )}
+          </div>
+
+          <div>
+            <label className="flex h-14 w-full bg-primary text-white cursor-pointer items-center justify-center rounded-md border-2 border-default-100 shadow-sm  transition-all duration-100 hover:border-default-400">
+              Upload Image:
+              <input
+                className="w-full hidden"
+                name="image"
+                type="file"
+                onChange={handleImage}
+              />
+            </label>
+          </div>
 
           <div className="w-1/2 mx-auto p-2">
             <Button className="w-full" size="lg" type="submit">
