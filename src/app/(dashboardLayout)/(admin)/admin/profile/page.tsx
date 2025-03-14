fca1ddb7f186fa7899e6ebc693f1f12cef5eb235
pantho0 <panthomashreky6@gmail.com>
2025-03-14 20:29:35 +0600

    added my posts in profile page

--- src/app/(dashboardLayout)/(admin)/admin/profile/page.tsx
+++ src/app/(dashboardLayout)/(admin)/admin/profile/page.tsx
@@ -1,21 +1,30 @@
 "use client";
 
+import PostCard from "@/src/components/home/PostCard";
 import Loading from "@/src/components/ui/Loading";
 import { useGetMe } from "@/src/hooks/auth.hooks";
+import { useGetMyPosts } from "@/src/hooks/post.hooks";
+import { IPost } from "@/src/types";
 import { Button } from "@heroui/button";
 import { Divider } from "@heroui/divider";
+import { Link } from "@heroui/link";
 import { Verified, VerifiedIcon } from "lucide-react";
 import Image from "next/image";
 import { use } from "react";
 
 const ProfilePage = () => {
   const { data, isLoading } = useGetMe();
+  const { data: posts, isLoading: isPostsLoading } = useGetMyPosts();
+
+  const allPosts = posts?.data;
+
+  console.log(allPosts);
 
   const user = data?.data;
 
   return (
     <>
-      {isLoading ? (
+      {isLoading || isPostsLoading ? (
         <Loading />
       ) : (
         <div className="max-w-7xl mx-auto p-10  mx-6 my-6 rounded-xl border-dotted border-2">
@@ -43,8 +52,12 @@ const ProfilePage = () => {
 
           <Divider className="my-8" />
 
-          <div>
-            <p>All Posts</p>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-6">
+            {allPosts?.map((post: IPost) => (
+              <Link key={post?._id} href={`/posts/${post?._id}`}>
+                <PostCard post={post} />
+              </Link>
+            ))}
           </div>
         </div>
       )}
