9a80f71e27c3ee814904db4bd8b52451e97b700c
pantho0 <panthomashreky6@gmail.com>
2025-03-28 00:32:11 +0600

    user delete hook added

--- src/hooks/user.hooks.ts
+++ src/hooks/user.hooks.ts
@@ -1,9 +1,15 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { getUsers } from "../services/user";
+import { deleteUser, getUsers } from "../services/user";
 
 export const useGetUsers = () => {
   return useQuery<any, Error, any>({
     queryKey: ["USER"],
     queryFn: async () => await getUsers(),
+  });
+};
+
+export const useDeleteUser = () => {
+  return useMutation<any, Error, any>({
+    mutationFn: async (email: string) => await deleteUser(email),
   });
 };
