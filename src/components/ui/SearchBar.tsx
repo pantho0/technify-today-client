dc41d1da02a8641b0296bdf6b6fcf07051c07411
pantho0 <panthomashreky6@gmail.com>
2025-04-19 14:06:26 +0600

    search input field markedup with react hook form

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -3,27 +3,38 @@
 import { Input } from "@heroui/input";
 import { Kbd } from "@heroui/kbd";
 import { SearchIcon } from "@/src/components/icons";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 const SearchBar = () => {
+  const { handleSubmit, watch, register } = useForm();
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log(data);
+  };
+
   return (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          {...register("search")}
+          aria-label="Search"
+          classNames={{
+            inputWrapper: "bg-default-100",
+            input: "text-sm",
+          }}
+          endContent={
+            <Kbd className="hidden lg:inline-block" keys={["command"]}>
+              K
+            </Kbd>
+          }
+          labelPlacement="outside"
+          placeholder="Search..."
+          startContent={
+            <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
+          }
+          type="search"
+        />
+      </form>
+    </>
   );
 };
 
