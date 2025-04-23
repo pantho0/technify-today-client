427dd47e8a9d20dbcb661dc2b7d54f2ff7a932c1
pantho0 <panthomashreky6@gmail.com>
2025-04-23 20:21:35 +0600

    infinite scrollbar added for feeds page

--- src/app/(commonLayout)/feeds/page.tsx
+++ src/app/(commonLayout)/feeds/page.tsx
@@ -1,12 +1,39 @@
+"use client";
 import { Button } from "@heroui/button";
 import { Link } from "@heroui/link";
 
 import PostCard from "@/src/components/home/PostCard";
-import getLatestPosts from "@/src/services/latestPosts";
-import { IPost } from "@/src/types";
 
-const FeedsPage = async () => {
-  const { data: posts } = await getLatestPosts();
+import { IPost } from "@/src/types";
+import { useEffect, useState } from "react";
+import { getAllPosts } from "@/src/services/post";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { Spinner } from "@heroui/spinner";
+
+const FeedsPage = () => {
+  const [posts, setPosts] = useState<IPost[]>([]);
+
+  console.log(posts);
+
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+
+  const fetchPosts = async () => {
+    const res = await getAllPosts(page);
+    const newPosts = res?.data?.result;
+
+    if (!newPosts || newPosts.length === 0) {
+      setHasMore(false);
+      return;
+    }
+
+    setPosts((prev) => [...prev, ...newPosts]);
+    setPage((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   if (!posts) {
     return <div>No posts found</div>;
@@ -22,9 +49,21 @@ const FeedsPage = async () => {
         </Link>
       </div>
 
-      <div className="grid grid-cols-1 w-2/4 mx-auto gap-10">
-        {posts?.result?.map((post: IPost) => <PostCard post={post} />)}
-      </div>
+      <InfiniteScroll
+        dataLength={posts.length}
+        next={fetchPosts}
+        hasMore={hasMore}
+        loader={<Spinner />}
+        endMessage={
+          <p className="text-center text-gray-500 my-4">
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        <div className="grid grid-cols-1 w-2/4 mx-auto gap-10">
+          {posts?.map((post: IPost, idx) => <PostCard key={idx} post={post} />)}
+        </div>
+      </InfiniteScroll>
     </div>
   );
 };
