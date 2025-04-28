c1c7a3ad747b2b50c189d05cd0bfb79fe0655a35
pantho0 <panthomashreky6@gmail.com>
2025-04-28 19:28:31 +0600

    all navs links are added to mobile device

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -93,22 +93,19 @@ export const Navbar = () => {
         <NavbarMenu>
           <SearchBar />
           <div className="mx-4 mt-2 flex flex-col gap-2">
-            {siteConfig.navItems.map((item, index) => (
-              <NavbarMenuItem key={`${item}-${index}`}>
-                <Link
-                  color={
-                    index === 2
-                      ? "primary"
-                      : index === siteConfig.navItems.length - 1
-                        ? "danger"
-                        : "foreground"
-                  }
-                  href="#"
-                  size="lg"
+            {siteConfig.navItems.map((item) => (
+              <NavbarItem key={item.href}>
+                <NextLink
+                  className={clsx(
+                    linkStyles({ color: "foreground" }),
+                    "data-[active=true]:text-primary data-[active=true]:font-medium"
+                  )}
+                  color="foreground"
+                  href={item.href}
                 >
                   {item.label}
-                </Link>
-              </NavbarMenuItem>
+                </NextLink>
+              </NavbarItem>
             ))}
           </div>
         </NavbarMenu>
