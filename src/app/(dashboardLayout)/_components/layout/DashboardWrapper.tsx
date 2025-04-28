99bf3174c70d0fc7587d4c9f18cdeb15013a2dcf
pantho0 <panthomashreky6@gmail.com>
2025-04-28 11:55:52 +0600

    position changed for dashbaoard menu trigger button and changed the icon

--- src/app/(dashboardLayout)/_components/layout/DashboardWrapper.tsx
+++ src/app/(dashboardLayout)/_components/layout/DashboardWrapper.tsx
@@ -2,6 +2,7 @@
 import { ReactNode, useState } from "react";
 
 import DashboardSidebar from "../DashboardSidebar";
+import { SquareMenu, X } from "lucide-react";
 
 export const DashboardWrapper = ({ children }: { children: ReactNode }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -13,18 +14,18 @@ export const DashboardWrapper = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex h-screen">
       <div
-        className={`fixed border-r border-gray-200 bg-foreground-100/30 lg:relative lg:w-1/5 h-screen overflow-y-auto overflow-x-hidden transform transition-transform duration-300 ease-in-out ${
+        className={`fixed border-r border-gray-200 bg-foreground-100 lg:relative lg:w-1/5 h-screen overflow-y-auto overflow-x-hidden transform transition-transform duration-300 ease-in-out ${
           isSidebarOpen ? "translate-x-0" : "-translate-x-full"
         } lg:translate-x-0 z-10`}
       >
         <DashboardSidebar />
       </div>
-      <div className="w-full lg:w-4/5 h-screen overflow-y-auto">
+      <div className="w-full bg-foreground-100/20 lg:w-4/5 h-screen overflow-y-auto">
         <button
-          className="lg:hidden p-2 bg-gray-800 text-white fixed top-2 left-2 z-20"
+          className="lg:hidden p-2 bg-gray-800 text-white fixed top-2 right-2 z-20"
           onClick={toggleSidebar}
         >
-          {isSidebarOpen ? "Close" : "Open"} Sidebar
+          {isSidebarOpen ? <X /> : <SquareMenu />}
         </button>
 
         {children}
