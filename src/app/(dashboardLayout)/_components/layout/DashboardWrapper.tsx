11281083f8f3d3703536e700e9b70800574e7cf8
pantho0 <panthomashreky6@gmail.com>
2025-04-29 17:46:45 +0600

    unecessary codes removed

--- src/app/(dashboardLayout)/_components/layout/DashboardWrapper.tsx
+++ src/app/(dashboardLayout)/_components/layout/DashboardWrapper.tsx
@@ -13,7 +13,6 @@ export const DashboardWrapper = ({ children }: { children: ReactNode }) => {
 
   const closeSidebar = () => setIsSidebarOpen(false);
 
-  // Detect clicks outside sidebar
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (
@@ -37,18 +36,15 @@ export const DashboardWrapper = ({ children }: { children: ReactNode }) => {
 
   return (
     <div className="relative flex h-screen overflow-hidden">
-      {/* Sidebar */}
       <div
         ref={sidebarRef}
         className={`fixed border-r border-gray-200 bg-foreground-100 lg:relative lg:w-1/5 h-screen overflow-y-auto transform transition-transform duration-300 ease-in-out z-30 ${
           isSidebarOpen ? "translate-x-0" : "-translate-x-full"
         } lg:translate-x-0`}
       >
-        {/* Close sidebar on link click */}
         <DashboardSidebar />
       </div>
 
-      {/* Blurred Overlay */}
       {isSidebarOpen && (
         <div
           className="lg:hidden fixed inset-0 bg-black/30 backdrop-blur-sm z-20"
@@ -56,7 +52,6 @@ export const DashboardWrapper = ({ children }: { children: ReactNode }) => {
         />
       )}
 
-      {/* Main Content */}
       <div className="w-full bg-foreground-100/20 lg:w-4/5 h-screen overflow-y-auto relative z-10">
         <button
           className="lg:hidden p-2 bg-gray-800 text-white fixed top-2 right-2 z-40"
