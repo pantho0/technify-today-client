397456844a2a8165b3d410d709838069cb2d9d20
pantho0 <panthomashreky6@gmail.com>
2025-05-04 11:58:26 +0600

    same design and functionalities applied to user profile page

--- src/app/(dashboardLayout)/(user)/user/profile/page.tsx
+++ src/app/(dashboardLayout)/(user)/user/profile/page.tsx
@@ -10,60 +10,45 @@ import { VerifiedIcon } from "lucide-react";
 import Image from "next/image";
 import ChangePasswordModal from "../../../_components/modals/ChangePassword";
 import UploadImageModal from "../../../_components/modals/UploadImageModal";
-import { useEffect, useState } from "react";
-import { getMyPosts } from "@/src/services/post";
-import InfiniteScroll from "react-infinite-scroll-component";
-import { Spinner } from "@heroui/spinner";
+import { useGetMyPosts } from "@/src/hooks/post.hooks";
+import { useEffect, useRef, useState } from "react";
+import { Pagination } from "@heroui/pagination";
+import PostSkeleton from "@/src/components/ui/PostSkeleton";
 
 const ProfilePage = () => {
   const { data, isLoading } = useGetMe();
-  const [posts, setPosts] = useState<IPost[]>([]);
-  const [page, setPage] = useState(1);
-  const [isAllPostLoaded, setIsAllPostLoaded] = useState(false);
-  const [hasMore, setHasMore] = useState(true);
-
-  const fetchPosts = async (pageNum: number) => {
-    try {
-      const res = await getMyPosts(page);
-      const newPosts = res?.data?.result;
-
-      if (!newPosts || newPosts.length === 0) {
-        setIsAllPostLoaded(true);
-        setHasMore(false);
-        return;
-      }
-
-      setPosts((prev) => {
-        const existingPosts = new Set(prev.map((post) => post._id));
-        const filteredNewPosts = newPosts.filter(
-          (post: any) => !existingPosts.has(post._id)
-        );
-        return [...prev, ...filteredNewPosts];
-      });
-
-      setPage(pageNum + 1);
-    } catch (error) {
-      console.error("Error fetching posts:", error);
-      setHasMore(false);
-    }
-  };
-
   const user = data?.data;
+  const [currentPage, setCurrentPage] = useState(1);
+  const topRef = useRef<HTMLDivElement>(null);
+
+  const {
+    mutate: handleGetMyPosts,
+    data: myPostsData,
+    isPending: isMyPostsPending,
+    isSuccess: isMyPostsSuccess,
+  } = useGetMyPosts();
 
   useEffect(() => {
-    setPage(1);
-    setIsAllPostLoaded(false);
-    setHasMore(true);
-    setPosts([]);
-    fetchPosts(1);
-  }, []);
+    handleGetMyPosts(currentPage);
+  }, [currentPage]);
+
+  const posts = myPostsData?.data?.result;
+
+  useEffect(() => {
+    if (topRef.current) {
+      topRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  }, [currentPage]);
 
   return (
-    <>
+    <div className="border m-3 rounded-3xl">
       {isLoading ? (
         <Loading />
       ) : (
-        <div className="max-w-7xl mx-2 p-2  md:p-6  md:mx-6 my-6 rounded-xl border-dotted border-2">
+        <div
+          ref={topRef}
+          className="max-w-7xl mx-2 p-2  md:p-6  md:mx-6 rounded-xl"
+        >
           <div className="flex items-center justify-center mb-4">
             <Image
               alt="user"
@@ -86,46 +71,37 @@ const ProfilePage = () => {
             <UploadImageModal />
           </div>
 
-          <Divider className="my-8" />
-          <div>
-            <InfiniteScroll
-              dataLength={posts.length}
-              next={() => fetchPosts(page)}
-              hasMore={hasMore}
-              refreshFunction={() => fetchPosts(page)}
-              pullDownToRefreshThreshold={90}
-              pullDownToRefreshContent={
-                <h3 style={{ textAlign: "center" }}>
-                  &#8595; Pull down to refresh
-                </h3>
-              }
-              releaseToRefreshContent={
-                <h3 style={{ textAlign: "center" }}>
-                  &#8593; Release to refresh
-                </h3>
-              }
-              loader={
-                <div className="w-full flex justify-center py-4">
-                  <Spinner />
-                </div>
-              }
-              endMessage={
-                <p className="text-center text-gray-500 my-4">
-                  <b>No more data to load!!</b>
-                </p>
-              }
-              scrollThreshold={0.7}
-            >
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 my-6">
-                {posts.map((post: IPost) => (
-                  <PostCard key={post._id} post={post} />
-                ))}
-              </div>
-            </InfiniteScroll>
-          </div>
+          <Divider className="my-4" />
         </div>
       )}
-    </>
+      <div className="max-w-7xl mx-2 md:mx-6 rounded-xl ">
+        {isMyPostsPending ? (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 px-4 gap-4 my-6">
+            {[...Array(9)].map((_, idx) => (
+              <PostSkeleton key={idx} />
+            ))}
+          </div>
+        ) : (
+          <>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 px-4">
+              {posts?.map((post: any) => (
+                <PostCard key={post?._id} post={post} />
+              ))}
+            </div>
+            <div className="flex justify-center my-6">
+              <Pagination
+                showControls
+                onChange={(page: number) => {
+                  setCurrentPage(page);
+                }}
+                initialPage={currentPage}
+                total={myPostsData?.data?.meta?.totalPage}
+              />
+            </div>
+          </>
+        )}
+      </div>
+    </div>
   );
 };
 
